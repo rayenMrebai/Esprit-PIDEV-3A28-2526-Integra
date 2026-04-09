90989f10230f85adacc1478fab84819b8ede972f
zakaria-bk <boukardougha.medzakaria@esprit.tn>
2026-04-09 12:59:15 +0100

    Complete user management with authentication, roles, filter, chart, PDF export

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,8 @@
-import './bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
-import './styles/app.css';
+import 'bootstrap';
+//import '@fortawesome/fontawesome-free/css/all.min.css';
+import './styles/app.scss';
+
+// Le CSS d'AdminLTE sera importé depuis app.scss (voir ci-dessous)
+// On évite l'import direct ici pour éviter l'erreur de résolution.
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
